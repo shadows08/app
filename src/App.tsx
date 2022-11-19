f6ca5bb7e9cb5a3cc54f8bf92485af8be3393460
Victor <matt_shadows08@hotmail.com>
2022-11-19 17:00:23 +0100

    edit 3

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React edit2
+          Learn React edit3
         </a>
       </header>
     </div>
